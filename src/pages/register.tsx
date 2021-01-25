43f42064884d6f6f0b1d3f9f4de59ea4fbe82a80
Luka <l.bjelica@globalsoft.ba>
2021-01-25 16:54:35 +0100

    Web register.

---
+++ src/pages/register.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import {Form, Formik} from 'formik'
+import { FormControl, FormLabel, Input, FormErrorMessage, Box, Button } from "@chakra-ui/react";
+import {Wrapper} from "../components/Wrapper";
+import { InputField } from "../components/InputField";
+
+interface registerProps {}
+
+
+
+
+const Register: React.FC<registerProps> = ({}) => {
+    return (
+    <Wrapper variant="small">
+    <Formik 
+        
+    initialValues={{username: "", password: ""}} 
+    onSubmit={(values) => {
+        console.log(values); 
+    }}>
+
+    {({isSubmitting}) => (
+
+    <Form>
+            <InputField 
+            name='username'
+            placeholder='username'
+            label='Username'
+            />
+            <Box mt={4}>
+            <InputField
+            name='password'
+            placeholder='password'
+            label='Password'
+            type='password'
+            />
+            </Box>
+            <Box mt={4}>
+            <Button
+            type='submit'
+            isLoading= {isSubmitting}
+            colorScheme='teal'>
+                Register
+            </Button>
+            </Box>
+          
+    </Form>
+    )}
+    </Formik>
+    </Wrapper>
+    );
+};
+
+export default Register;
