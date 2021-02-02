3fdd0addaad2ca12289f1c027b13f30c76a8d5ce
Luka <l.bjelica@globalsoft.ba>
2021-02-02 20:52:17 +0100

    Global erroro handling

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,22 +1,28 @@
 
-import { FormControl, FormLabel, Input, FormErrorMessage } from '@chakra-ui/react';
+import { FormControl, FormErrorMessage, FormLabel, Input, Textarea } from '@chakra-ui/react';
 import { useField } from 'formik';
-import React, { InputHTMLAttributes } from 'react'
+import React, { InputHTMLAttributes } from 'react';
 
 type InputFieldProps =  InputHTMLAttributes<HTMLInputElement> & {
     label: string;
     name: string;
+    textarea?: boolean
 };
 
 export const InputField: React.FC<InputFieldProps> = ({
-    label, 
+    label,
+    textarea, 
     size: _,  // _ <- unused variable
     ...props}) => {
+    let InputOrTextarea: any = Input;
+    if (textarea) {
+        InputOrTextarea = Textarea;
+    }
     const [field, {error}] = useField(props);
     return (
         <FormControl isInvalid={!!error}>
         <FormLabel htmlFor={field.name}>{label}</FormLabel>
-        <Input 
+        <InputOrTextarea 
         {...field} 
         {...props} 
         id={field.name} 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,24 @@
-import { NavBar } from "../components/NavBar";
 import { withUrqlClient } from 'next-urql';
 import { createUrqlClient } from "../utils/createUrqlClient";
+import { usePostsQuery } from "../generated/graphql";
+import { Layout } from "../components/Layout";
+import { Link } from '@chakra-ui/react';
+import NextLink from 'next/link';
 
- const Index = () => (
-   <>
-   <NavBar></NavBar>
-  <div>hello</div>
-  </>
+ const Index = () => {
+   const [{data}] = usePostsQuery();
+   return(
+    <Layout>
+      
+        <NextLink href='/create-post'>
+          <Link>create post</Link>
+        </NextLink>
+      
+      <br />
+     {!data ? null : data.posts.map(p => <div key={p.id}>{p.title}</div>)}
+   </Layout>
 );
+}
 
 export default withUrqlClient(createUrqlClient)(Index);
  
